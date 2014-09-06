67dcbf65c10e478f115ab2a5b328552139a543d3
Cy <whatever>
2014-09-06 11:11:59 -0700

    auto 212 646 5571

--- checkup.lua
+++ checkup.lua
@@ -88,6 +88,7 @@ end
 local gi = require('lgi')
 local gtk = gi.require('Gtk')
 local glib = gi.require('GLib')
+local gdk = gi.require('Gdk')
 
 local window = gtk.Window {
     title = 'Checkup on Friends',
@@ -98,6 +99,8 @@ local window = gtk.Window {
         db.close()
     end
 }
+
+window:stick()
 
 local vbox = gtk.VBox()
 local grid = gtk.Grid()
@@ -112,8 +115,17 @@ local intervals = (function ()
         end,
         get = function(key)
             local entry = store[key.id]
+            if not entry then return end
             entry[1] = key
             return entry[2]
+        end,
+        check = function(key)
+            local entry = store[key.id]
+            if not entry then return false end
+            local oldkey = entry[1]
+            if oldkey.dirty then return false end
+            store[key.id] = {key,entry[2]}
+            return true
         end,
         clear = function()
             store = {}
@@ -131,25 +143,70 @@ end)()
 
 local function catch(sub) 
     return function()
-        ok,err = pcall(sub)
-        if not ok then 
-            print('error',err) 
+        ok,err = xpcall(sub, function(err)
+            print('*********')
+            print(debug.traceback(err,3))
+            print('*********')
+            return err
+        end)
+        if not ok then
+            os.exit(3)
         end
-        assert(ok,err)
         return err
     end
 end
 
+local colorFor = (function()
+    return function(ratio)
+        local r,g,b
+        b = 0
+        if ratio >= 1 then
+            g = 0
+            r = 1
+        elseif ratio <= -1 then
+            g = 1
+            r = 0
+        else
+            -- m = (1 - 0) / (-1 - 1) = -1/2
+            -- 1 = -1/2*-1+b, b = 1/2
+            g = -0.5 * (ratio - 1)
+            r = 0.5 * (ratio + 1)
+        end
+        color = gdk.RGBA()
+        color.red = r
+        color.green = g
+        color.blue = b
+        color.alpha = 1
+        return color
+    end
+end)()
+
+local black = gdk.RGBA()
+black.red = 0
+black.green= 0
+black.blue = 0
+black.alpha = 1
+
+local creating = false
 local function createGrid()
+    if creating then glib.source_remove(creating) end
     intervals.clear()
     grid:forall(gtk.Widget.destroy)
-    glib.idle_add(glib.PRIORITY_DEFAULT_IDLE,catch(function()
-        i = 0
+    print('size',grid:get_size_request())
+    creating = glib.idle_add(glib.PRIORITY_DEFAULT_IDLE,catch(function()
+        local i = 0
         for habit in db.pending() do
             grid:insert_row(i)
             grid:attach(gtk.Label{label=habit.description},0,i,1,1);
             (function (habit,i)
-                local intervalLabel = gtk.Label{label=interval(habit.elapsed)}
+                local intervalLabel = gtk.Label{
+                    label=interval(habit.elapsed),
+                }
+                if habit.elapsed ~= nil then
+                    intervalLabel:override_color(gtk.StateFlags.NORMAL,colorFor((habit.elapsed - habit.frequency) / habit.frequency))
+                else
+                    intervalLabel:override_color(gtk.StateFlags.NORMAL,black)
+                end
                 intervals.set(habit,intervalLabel)
                 -- never using contacted in a callback
                 -- no need to save in the environment
@@ -160,7 +217,8 @@ local function createGrid()
                         db.transaction(function()
                             habit.perform()
                         end)
-                        intervalLabel:set_text('just now')
+                        intervalLabel:set_text('just now')                        
+                        createGrid()
                     end)
                     if not ok then print(err,'err') end
                     assert(ok,err)
@@ -174,10 +232,12 @@ local function createGrid()
                     glib.idle_add(glib.PRIORITY_DEFAULT_IDLE,createGrid)
                 end 
                 grid:attach(disable,3,i,1,1)
+                print('size2',grid:get_size_request())
             end)(habit,i)
             i = i + 1
         end
         grid:show_all()
+        creating = false
         return false
     end))
 end
@@ -185,17 +245,24 @@ createGrid()
 
 local function updateIntervals()
     for habit in db.pending() do
-        local label = intervals.get(habit)
-        if label == nil then
+        if not intervals.check(habit) then
             createGrid()
             return true
         end
-        if habit.hasperformed and (habit.elapsed == nil) then
-            createGrid()
-            return true
-        end
+        label = intervals.get(habit)
         label:set_text(interval(habit.elapsed))
+        if habit.elapsed ~= nil then
+            label:override_color(gtk.StateFlags.NORMAL,colorFor((habit.elapsed - habit.frequency) / habit.frequency))
+        else
+            label:override_color(gtk.StateFlags.NORMAL,black)
+        end
     end
+    return true
+end
+
+local function raiseWindow()
+    window:present()
+    glib.idle_add(glib.PRIORITY_DEFAULT,catch(function() window:grab_focus() end))
     return true
 end
 
@@ -205,6 +272,7 @@ end
     local function start()
         assert(handle == nil)
         handle = glib.timeout_add_seconds(glib.PRIORITY_DEFAULT,1,updateIntervals)
+        glib.timeout_add_seconds(glib.PRIORITY_DEFAULT,1800,raiseWindow)
     end
     local function stop()
         glib.source_remove(handle)
