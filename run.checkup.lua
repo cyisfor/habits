f6203ad2a4d356c350cfaa60435636d260d2a14d
user <nobody>
2016-03-12 07:42:22 +0000

    ugh, run was randomly failing
    when called in the current directory nil pointer error

--- run.checkup.lua
+++ run.checkup.lua
@@ -4,9 +4,14 @@ require('luarocks.loader')
 here = debug.getinfo(1,"S").source
 assert(here:sub(1,1)=='@')
 here = here:sub(2)
-local slash = #here - here:reverse():find('/')+1
-here = here:sub(0,slash-1)
+local hsals = here:reverse():find('/')
+if hsals == nil then
+   here = "."
+else
+   local slash = #here - here:reverse():find('/')+1
+   here = here:sub(0,slash-1)
+end
 package.path = package.path .. ';'..here..'/?.lua'
 package.cpath = package.cpath .. ';'..here..'/?.so'
-
+print(here)
 require('checkup')
