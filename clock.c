c2323774a94d604e29eb49e8f9d455512302a034
user <nobody>
2016-08-31 20:49:46 +0000

    auto (clock.c) 118 947

--- clock.c
+++ clock.c
@@ -1,18 +1,4 @@
-void timespec_exact_divide(struct timespec* numerator, struct timespec* denominator) {
-	// returns numerator-> divisor / denominator -> modulus
-	// assumes divisor is "small" (for clock_getres)
-	int denom = denominator->tv_sec * 1000000000 + denominator->tv_nsec;
-	long smod = (numerator->tv_sec % denom) * (1000000000 % denom);
-	long sdiv = numerator->tv_sec * (1000000000 / denom);
-	long nmod = numerator->tv_nsec % denom;
-	long ndiv = numerator->tv_nsec / denom;
-	numerator->tv_sec = sdiv;
-	numerator->tv_nsec = ndiv;
-	denominator->tv_sec = smod;
-	denominator->tv_nsec = nmod;
-}
-
-long timespec_divide(struct timespec* numerator,
+long timespec_divide(const struct timespec* numerator,
 										 const struct timespec* denominator) {
 	// returns units of denominator in numerator
 	assert(denominator->tv_sec == 0); // not THAT imprecise!
@@ -23,4 +9,13 @@ long timespec_divide(struct timespec* numerator,
 	}
 	res += numerator.tv_sec / denominator.tv_nsec;
 	return res;
+}
+
+void timespec_multiply(struct timespec* result,
+											 long quantity,
+											 const struct timespec* unit) {
+	assert(unit->tv_sec == 0); // not THAT imprecise!
+	result->tv_sec = (quantity * unit->tv_nsec) / 1000000000L ||
+		(quantity / 1000000000L) * unit->tv_nsec;
+	result->tv_nsec = (quantity * unit.tv_nsec) % 1000000000L;
 }
