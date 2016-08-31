c2085433c7d1de8e0655f9b7a9f134e6a6f3be4c
user <nobody>
2016-08-31 22:37:59 +0000

    auto (make_embed.c) 437 4859

--- checkup.lua
+++ checkup.lua
@@ -5,103 +5,103 @@ local db = require('db')
 print('\x1b]0;checkup\a')
 
 function table.join(delim,t)
-    local result = nil
-    for _,v in ipairs(t) do
-        if result == nil then
-            result = v
-        else
-            result = result .. delim .. v
-        end
-    end
-    return result
+	 local result = nil
+	 for _,v in ipairs(t) do
+			if result == nil then
+				 result = v
+			else
+				 result = result .. delim .. v
+			end
+	 end
+	 return result
 end
 
 function interval(i)
-    ok,err = pcall(function()
-        if i == nil then
+	 ok,err = pcall(function()
+				 if i == nil then
             return 'never'
-        elseif type(i) == 'string' then
+				 elseif type(i) == 'string' then
             return i
-        end
-        local result = ''
-        local unit
+				 end
+				 local result = ''
+				 local unit
 
-        local function addSpace()
+				 local function addSpace()
             if result == nil then
-                result = ''
+							 result = ''
             else
-                result = result .. ' '
+							 result = result .. ' '
             end
-        end
-            
-        local function addNamed(one,many)
+				 end
+				 
+				 local function addNamed(one,many)
             return function(things)
-                addSpace()
-                
-                if things == 1 then
-                    result = result .. things..' '..one
-                elseif things == 0 then
-                else
-                    result = result .. things..' '..(many or (one..'s'))
-                end
+							 addSpace()
+							 
+							 if things == 1 then
+									result = result .. things..' '..one
+							 elseif things == 0 then
+							 else
+									result = result .. things..' '..(many or (one..'s'))
+							 end
             end
-        end
-        local function addSpaced(space)
+				 end
+				 local function addSpaced(space)
             return function(things)
-                if things < 10 then
-                    things = space.. things
-                end
-                result = result .. things
+							 if things < 10 then
+									things = space.. things
+							 end
+							 result = result .. things
             end
-        end
-        
-        addMinutes = (function(add)
-            return function(minutes)
-                result = result .. ':'
-                add(minutes)
-            end
-        end)(addSpaced('0'));
+				 end
+				 
+				 addMinutes = (function(add)
+							 return function(minutes)
+									result = result .. ':'
+									add(minutes)
+							 end
+											end)(addSpaced('0'));
 
-        addHours = (function (add)
-            return function(hours)
-                if hours == 0 then return end
---                addSpace()
---                add(hours)
-				result = result .. hours
-            end
-        end)(addSpaced(' '));
+				 addHours = (function (add)
+							 return function(hours)
+									if hours == 0 then return end
+									--                addSpace()
+									--                add(hours)
+									result = result .. hours
+							 end
+										end)(addSpaced(' '));
 
-        local function produce(i,multiple,add)
+				 local function produce(i,multiple,add)
             things,i = math.modf(i*multiple)
             add(things)
             return i,things
-        end
-        i = i / 86400 -- postgresql won't give me julian days >:( 
-        i,days = produce(i,1,addNamed('day'))
-        if i > 0 then
+				 end
+				 i = i / 86400 -- postgresql won't give me julian days >:( 
+				 i,days = produce(i,1,addNamed('day'))
+				 if i > 0 then
             if days <= 10 then
-			   if days > 1 then
-				  addSpace()
-			   end
-                i = produce(i,24,addHours)
-                if days <= 1 then
-                    i = produce(i,60,addMinutes)
-                else
-                    result = result .. 'h'
-                end
+							 if days > 1 then
+									addSpace()
+							 end
+							 i = produce(i,24,addHours)
+							 if days <= 1 then
+									i = produce(i,60,addMinutes)
+							 else
+									result = result .. 'h'
+							 end
             end
-        end
+				 end
 
-        return result.. ' '
-    end)
-    if not ok then
-        print('I is',i)
-        for n,v in pairs(i) do
-            print(n,v)
-        end
-        assert(ok,err)
-    end
-    return err
+				 return result.. ' '
+	 end)
+	 if not ok then
+			print('I is',i)
+			for n,v in pairs(i) do
+				 print(n,v)
+			end
+			assert(ok,err)
+	 end
+	 return err
 end
 
 local gi = require('lgi')
@@ -136,56 +136,56 @@ end
 math.randomseed(os.time())
 
 local function catch(sub) 
-    return function()
-        ok,err = xpcall(sub, function(err)
-            print('*********')
-            print(debug.traceback(err,3))
-            print('*********')
-            return err
-        end)
-        if not ok then
-            os.exit(3)
-        end
-        return err
-    end
+	 return function()
+			ok,err = xpcall(sub, function(err)
+												 print('*********')
+												 print(debug.traceback(err,3))
+												 print('*********')
+												 return err
+			end)
+			if not ok then
+				 os.exit(3)
+			end
+			return err
+	 end
 end
 
 local colorFor = (function()
-    return function(ratio)
-	   if ratio == nil then return black end
-        local r,g,b
-        b = 0
-        if ratio >= 1 then
+			return function(ratio)
+				 if ratio == nil then return black end
+				 local r,g,b
+				 b = 0
+				 if ratio >= 1 then
             g = 0
             r = 1
-        elseif ratio <= -1 then
+				 elseif ratio <= -1 then
             g = 1
             r = 0
-        else
+				 else
             -- m = (1 - 0) / (-1 - 1) = -1/2
             -- 1 = -1/2*-1+b, b = 1/2
             g = -0.5 * (ratio - 1)
             r = 0.5 * (ratio + 1)
-        end
-        color = gdk.RGBA()
-        color.red = r
-        color.green = g
-        color.blue = b
-        color.alpha = 1
-        return color
-    end
-end)()
+				 end
+				 color = gdk.RGBA()
+				 color.red = r
+				 color.green = g
+				 color.blue = b
+				 color.alpha = 1
+				 return color
+			end
+								 end)()
 
 local function color(r,g,b,a)
    local res = gdk.RGBA()
    if g == nil then
-	  g = r
+			g = r
    end
    if b == nil then
-	  b = r
+			b = r
    end
    if a == nil then
-	  a = 1.0
+			a = 1.0
    end
    res.red = r
    res.green = g
@@ -193,7 +193,7 @@ local function color(r,g,b,a)
    res.alpha = a
    return res
 end
-   
+
 local black = color(0)
 local grey = color(0.95)
 local white = color(1.0)
@@ -205,12 +205,12 @@ local function raiseWindow()
    local label,habit = row[c.NAME],row[c.IDENT]
    local n = notify.Notification.new('',label,"task-due")
    function n:on_closed(e)
-	  local reason = n['closed-reason']
-	  --print('closed notify',reason)
-	  if reason == 2 then
-		 window:present()
-		 glib.idle_add(glib.PRIORITY_DEFAULT,catch(function() window:grab_focus() end))
-	  end
+			local reason = n['closed-reason']
+			--print('closed notify',reason)
+			if reason == 2 then
+				 window:present()
+				 glib.idle_add(glib.PRIORITY_DEFAULT,catch(function() window:grab_focus() end))
+			end
    end
    n:show()
    return true
@@ -219,58 +219,58 @@ end
 local function update_intervals()
    if creating then glib.source_remove(creating) end
    creating = glib.idle_add(glib.PRIORITY_DEFAULT_IDLE,catch(function()
-        creating = nil								   
-        local i = 0
-		local row = items:get_iter_first()
+																	creating = nil								   
+																	local i = 0
+																	local row = items:get_iter_first()
 
-        for habit,description,frequency,elapsed in db.pending() do
-		   local thingy
-		   if elapsed ~= nil then
-			  thingy = (elapsed - frequency) / frequency
-		   else
-			  thingy = nil
-		   end
-		   i = i + 1
-		   if row ~= nil then
-			  local function set(column,value)
-				 column = column - 1
-				 -- stoled from lgi/override/Gtk.lua
-				 value = GObject.Value(items:get_column_type(column),value)
-				 items:set_value(row, column, value)
-			  end
-			  set(c.IDENT,habit)
-			  set(c.ELAPSED,interval(elapsed))
-			  set(c.DANGER,colorFor(thingy))
-			  set(c.DISABLED,false)
-			  set(c.NAME,description)
-			  if not items:iter_next(row) then
-				 row = nil
-			  end
-		   else
-			  local background
-			  if i % 2 == 0 then
-				 background = white
-			  else
-				 background = grey
-			  end
-			  items:insert(-1, {
-							  [c.NAME] = description,
-							  [c.ELAPSED] = interval(elapsed),
-							  [c.DISABLED] = false,
-							  [c.DANGER] = colorFor(thingy),
-							  [c.IDENT] = habit,
-							  [c.BACKGROUND] = background
-			  })
-		   end
-		end		
-		while row do
-		   items:remove(row)
-		   if not items:iter_next(row) then
-			  break
-		   end
-		end
-        return false
-	end))
+																	for habit,description,frequency,elapsed in db.pending() do
+																		 local thingy
+																		 if elapsed ~= nil then
+																				thingy = (elapsed - frequency) / frequency
+																		 else
+																				thingy = nil
+																		 end
+																		 i = i + 1
+																		 if row ~= nil then
+																				local function set(column,value)
+																					 column = column - 1
+																					 -- stoled from lgi/override/Gtk.lua
+																					 value = GObject.Value(items:get_column_type(column),value)
+																					 items:set_value(row, column, value)
+																				end
+																				set(c.IDENT,habit)
+																				set(c.ELAPSED,interval(elapsed))
+																				set(c.DANGER,colorFor(thingy))
+																				set(c.DISABLED,false)
+																				set(c.NAME,description)
+																				if not items:iter_next(row) then
+																					 row = nil
+																				end
+																		 else
+																				local background
+																				if i % 2 == 0 then
+																					 background = white
+																				else
+																					 background = grey
+																				end
+																				items:insert(-1, {
+																												[c.NAME] = description,
+																												[c.ELAPSED] = interval(elapsed),
+																												[c.DISABLED] = false,
+																												[c.DANGER] = colorFor(thingy),
+																												[c.IDENT] = habit,
+																												[c.BACKGROUND] = background
+																				})
+																		 end
+																	end		
+																	while row do
+																		 items:remove(row)
+																		 if not items:iter_next(row) then
+																				break
+																		 end
+																	end
+																	return false
+	 end))
    return true
 end
 local function complete_row(items, path, iter)
@@ -294,33 +294,33 @@ end
 raiseWindow()
 
 ;(function()
-    local handle,raiser
-    local function start()
-        assert(handle == nil)
-        handle = glib.timeout_add_seconds(glib.PRIORITY_DEFAULT,
-										  1,update_intervals)
-        raiser = glib.timeout_add_seconds(glib.PRIORITY_DEFAULT,
-										  600,raiseWindow)
-    end
-    local function stop()
-        glib.source_remove(handle)
-        handle = nil
-		glib.source_remove(raiser)
-		raiser = nil
-    end
-    
-    local update = b.update
-    function update:on_toggled()
-        if update.active then
+			local handle,raiser
+			local function start()
+				 assert(handle == nil)
+				 handle = glib.timeout_add_seconds(glib.PRIORITY_DEFAULT,
+																					 1,update_intervals)
+				 raiser = glib.timeout_add_seconds(glib.PRIORITY_DEFAULT,
+																					 600,raiseWindow)
+			end
+			local function stop()
+				 glib.source_remove(handle)
+				 handle = nil
+				 glib.source_remove(raiser)
+				 raiser = nil
+			end
+			
+			local update = b.update
+			function update:on_toggled()
+				 if update.active then
             start()
-        else
+				 else
             stop()
-        end
-    end
-    update.active = true
-	update_intervals()
-	start()
-end)()
+				 end
+			end
+			update.active = true
+			update_intervals()
+			start()
+ end)()
 
 local view = b.view
 
--- src/checkup.c
+++ src/checkup.c
@@ -1,12 +1,211 @@
 #include "db.h"
 #include "checkup_glade.h"
 
-int int main(int argc, char *argv[])
+enum Column {
+	EHUNNO,
+	NAME,
+	ELAPSED,
+	DISABLED,
+	DANGER,
+	IDENT,
+	BACKGROUND
+};
+
+static void color_for(GdkRGBA* dest, float ratio) {
+	int r,g,b;
+	b = 0;
+	if(ratio >= 1) {
+		g = 0;
+		r = 1;
+	} else if(ratio <= -1) {
+		g = 1;
+		r = 0;
+	} else {
+		g = -0.5 * (ratio - 1);
+		r = 0.5 * (ratio + 1);
+	}
+	dest->red = r;
+	dest->green = g;
+	dest->blue = b;
+}
+
+#define COLOR(name, r,g,b,a) GdkRGBA name = { .red = r, .green = g, .blue = b, .alpha = a };
+
+COLOR(black,0,0,0,0);
+COLOR(grey,0.95,0.95,0.95,1.0);
+COLOR(white,1,1,1,1);
+
+int main(int argc, char *argv[])
 {
 	puts("\x1b]0;checkup\a");
 	fflush(stdout);
+	gtk_init(&argc,&argv);
+	
 	GtkBuilder* b = gtk_builder_new_from_string(checkup_glade,
 																							checkup_glade_length);
-	GtkWidget* top = GTK_WIDGET(gtk_builder_get_object(b, "top"));
+	#define GET(type,conv,name) \
+		type* name = conv(gtk_builder_get_object(b, #name))
+	#define GET(name) GET(GtkWidget, GTK_WIDGET, name)
+	GET(top);
+	GET(GtkTreeModel,GTK_TREE_MODEL,items);
+	GET(selection);
+	GET(didit);
+	GET(disabled);
+	GET(update);
+	GET(view);
+
+	gtk_window_stick(GTK_WINDOW(top));
+	g_signal_connect(top,"destroy",gtk_main_quit);
+
+	void on_notify_closed(NotifyNotification* n, gpointer udata) {
+		gint reason;
+		g_object_get(n,
+								 "closed-reason",
+								 &reason,
+								 NULL);
+		if(reason == 2) {
+			gtk_window_present(GTK_WINDOW(top));
+			gtk_widget_grab_focus(top);
+		}
+	}
+			
+	void poke_me() {
+		GtkTreeIter iter;
+		if(FALSE == gtk_tree_model_get_iter_first(items, &iter))
+			return;
+		GValue label, ident;
+		gtk_tree_model_get_value(items,
+														 &iter,
+														 0,
+														 &label);
+		gtk_tree_model_get_value(items,
+														 &iter,
+														 1,
+														 &ident);
+		NotifyNotification* n = notify_notification_new("", label,"task-due");
+		g_signal_connect(n, "closed", on_notify_closed, NULL);
+		gtk_widget_show_all(n);
+	}
+
+	void update_intervals() {
+		GtkTreeIter row;
+		bool has_row = gtk_tree_model_get_iter_first(items, &row);
+		struct db_habit habit;
+		GdkRGBA thingy;
+		while(db_pending(&habit)) {
+			gchar* elapsed = NULL;
+			interval_stringify(&elapsed, habit.elapsed);
+			if(habit.has_elapsed) {
+				color_for(&thingy, (habit.elapsed - frequency) / frequency);
+			}
+			
+			if(has_row) {
+				gtk_list_store_set(GTK_LIST_STORE(items),
+													 &row,
+													 IDENT, habit.ident,
+													 ELAPSED, elapsed,
+													 DISABLED, FALSE,
+													 NAME, habit.description
+													 -1);
+				has_row = gtk_tree_model_iter_next(items,&row);
+			} else {
+				gtk_list_store_set_values(GTK_LIST_STORE(items),
+																	&row,
+																	-1,
+																	IDENT, habit.ident,
+																	DISABLED, FALSE,
+																	BACKGROUND, background,
+																	NAME, habit.description,
+																	-1);
+			}
+			if(habit.has_elapsed) {
+				gtk_list_store_set(GTK_LIST_STORE(items),
+													 &row,
+													 DANGER, &thingy,
+													 -1);
+			}
+		}
+		// take off expired items at the end
+		while(has_row) {
+			has_row = gtk_list_store_remove(GTK_LIST_STORE(items), &row);
+		}
+	}
+
+	void complete_row(GtkTreeModel *model,
+										GtkTreePath *path,
+										GtkTreeIter *iter,
+										gpointer data) {
+		GValue ident;
+		gtk_tree_model_get_value(model, iter, EHUNNO, &ident);
+		assert(G_HOLDS_INT64(ident));
+		db_perform(g_value_get_int64(ident));
+	}
+
+	void on_didit() {
+		gtk_tree_model_foreach(items, complete_row, NULL);
+		update_intervals();
+	}
+	void disabled_toggled(gchar* spath) {
+		GtkTreePath* path = gtk_tree_path_new_from_string(spath);
+		GtkTreeIter iter;
+		assert(TRUE==gtk_tree_model_get_iter(items, &iter, path));
+		gtk_tree_path_free(path);
+		GValue disabledv;
+		gtk_tree_model_get_value(items,
+														 &iter,
+														 DISABLED,
+														 &disabledv);
+		assert(G_HOLDS_BOOL(disabledv));
+		gboolean disabled = g_value_get_bool(disabledv);
+		gtk_list_store_set_values(GTK_LIST_STORE(items),
+															&iter,
+															DISABLED,
+															disabled == FALSE ? TRUE : FALSE,
+															-1);
+		GValue ident;
+		gtk_tree_model_get_value(items,
+														 &iter,
+														 IDENT,
+														 &ident);
+		assert(G_HOLDS_INT64(ident));
+		db_set_enabled(g_value_get_int64(ident), disabled == FALSE);
+	}
+
+	void start() {
+		handle = g_timeout_add_seconds(PRIORITY_DEFAULT, 1, update_intervals);
+		poker = g_timeout_add_seconds(PRIORITY_DEFAULT, 600, poke_me);
+	}
+	void stop() {
+		g_source_remove(handle);
+		g_source_remove(raiser);
+		handle = -1;
+		raiser = -1;
+	}
+
+	void on_update_toggled() {
+		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(update)) == TRUE) {
+			start();
+		} else {
+			stop();
+		}
+	}
+	gtk_toggle_button_set_active(update, TRUE);
+	update_intervals();
+	start();
+	g_signal_connect(update, "toggled", on_update_toggled, NULL);
+
+	GtkCssProvider* css = gtk_css_provider_new();
+	gboolean ok =
+		gtk_css_provider_load_from_data(css,
+																	LITLEN("* { "
+																	"font-size: 14pt; "
+																	"font-weight: bold; "
+																	"}"),
+																	&error);
+	assert(ok == TRUE);
+	gtk_style_context_add_provider(gtk_widget_get_style_context(top),
+																 css,
+																 STYLE_PROVIDER_PRIORITY_APPLICATION);
+	gtk_main();
 	return 0;
 }
